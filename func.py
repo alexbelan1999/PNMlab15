a731f626fa4d56f782ea85659cbd19affd6ed091
alexbelan1999 <alexbelan1999@gmail.com>
2019-11-17 18:41:36 +0300

    Add newton,secant,chord method

--- func.py
+++ func.py
@@ -2,20 +2,20 @@ import numpy as np
 import math
 
 def tg(x):
-    return np.tan(0.4*x+0.4)-x*x
+    return math.tan(0.4*x+0.4)-x*x
     #return x*x*x - 3*x-0.4
 
 def deriv1(x):
-    return -2*x + 0.4/(math.cos(0.4*x+0.4)*math.cos(0.4*x+0.4))
+    return -2 * x + 0.4 / (math.cos(0.4 * x + 0.4) * math.cos(0.4 * x + 0.4))
 
 def deriv2(x):
-    return 0.32*math.sin(0.4*x+0.4)/pow(math.cos(0.4*x+0.4),3) - 2
+    return 0.32 * math.sin(0.4 * x + 0.4) / pow(math.cos(0.4 * x + 0.4), 3) - 2
 
 def table(a,b):
     x = []
     y = []
     print("Таблица")
-    for i in range(a,b+1):
+    for i in np.arange(a,b+0.25,0.25):
         x.append(i)
         y.append(tg(i))
     print("x:",x)
@@ -28,14 +28,14 @@ def dix(a,b,E):
 
     while ((b - a) / 2 >= E):
         c = (a + b) / 2
-        fa = math.copysign(1, tg(a))
-        fc = math.copysign(1, tg(c))
+        fa = tg(a)
+        fc = tg(c)
 
-        print(a, "  ", b, "  ", c, "  ", tg(c))
-        if (fa != fc):
-            b = c
-        else:
+        print(a, "  ", b, "  ", c, "  ", fc)
+        if (fc < 0 and fa < 0) or (fc > 0 and fa > 0):
             a = c
+        else:
+            b = c
         iter += 1
 
     print("Корень = ", c," с точностью ",E," количество итераций ",iter)
@@ -44,16 +44,43 @@ def dix(a,b,E):
 def newton(a,b,E):
     print("Метод Ньютона")
     iter = 0
-    x0 = 0
-    xn = 0
-    if (tg(a) * deriv2(a) != 0):
-        x0 = a
-    else :
-        x0 = b
-    while (abs(tg(x0) / deriv1(x0)) > E):
-        xn = x0 - tg(x0) / deriv1(x0)
-        print(x0, tg(x0), deriv1(x0), xn)
-        x0 = xn
+    x = a
+    x1 = x - tg(x) / deriv1(x)
+    while (abs(x1 - x) >= E) :
+        x = x1
+        x1 = x1 - tg(x1) / deriv1(x1)
+        print(x, "  ", x1, "  ", tg(x), "  ", deriv1(x))
         iter += 1
-    print("Корень = ", xn, " с точностью ", E, " количество итераций ", iter)
+    print("Корень = ", x1, " с точностью ", E, " количество итераций ", iter)
+    pass
+
+def secant(a,b,E):
+    print("Метод секущих")
+    iter = 0
+    x = a
+    x_1 = b
+    x1 = x - tg(x) / ((tg(x)-tg(x_1))/(x-x_1))
+    while (abs(x1 - x) >= E) :
+        x_1 = x
+        x = x1
+        x1 = x1 - tg(x1) / ((tg(x)-tg(x_1))/(x-x_1))
+        print(x_1, "  ", x, "  ", x1, "  ", tg(x), "  ", tg(x_1))
+        iter += 1
+    print("Корень = ", x1, " с точностью ", E, " количество итераций ", iter)
+
+    pass
+
+def chord(a,b,E):
+    print("Метод секущих")
+    iter = 0
+    x = a
+    x0 = a + E
+    x1 = x - tg(x) / ((tg(x) - tg(x0)) / (x - x0))
+    while (abs(x1 - x) >= E):
+        x = x1
+        x1 = x1 - tg(x1) / ((tg(x) - tg(x0)) / (x - x0))
+        print(x0, "  ", x, "  ", x1, "  ", tg(x), "  ", tg(x0))
+        iter += 1
+    print("Корень = ", x1, " с точностью ", E, " количество итераций ", iter)
+
     pass
--- lab15.py
+++ lab15.py
@@ -5,15 +5,17 @@ import math
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-x = np.arange(-2.0,2.0,0.01)
+x = np.arange(-1.0,2.5,0.01)
 y = np.tan(0.4*x+0.4)-x*x
 ax.plot(x, y, 'k')
 plt.title('График tg(0.4*x+0.4))=x^2')
 plt.grid(True)
+plt.xlabel('x')
+plt.ylabel('y')
 plt.show()
 
-a = -2
-b = 2
+a = -1
+b = 2.5
 E = 0.001
 
 func.table(a,b)
@@ -21,5 +23,8 @@ print()
 func.dix(a,b,E)
 print()
 func.newton(a,b,E)
-
+print()
+func.secant(a,b,E)
+print()
+func.chord(a,b,E)
 
